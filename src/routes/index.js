592d6243e35c4e57fff6a19ce04495991bcda85f
DYlan <gownerbeats@gmail.com>
2022-12-22 12:52:59 -0500

    sssssss

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,11 +10,11 @@ const { unlink } = require('fs-extra');
 const cloudinary = require('cloudinary');
 
 
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-})
+// cloudinary.config({
+//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//     api_key: process.env.CLOUDINARY_API_KEY,
+//     api_secret: process.env.CLOUDINARY_API_SECRET
+// })
 
 
 
